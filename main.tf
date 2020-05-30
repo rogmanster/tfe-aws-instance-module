95ba070d80da6b0297264d877b547805fd6820ce
Roger Chao <rogman@gmail.com>
2020-05-29 19:26:12 -0700

    First Commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+
+// Workspace Data
+data "terraform_remote_state" "aws_vpc_prod" {
+  backend = "remote"
+
+  config = {
+    organization = "rogercorp"
+    workspaces = {
+      name = "aws-vpc-prod"
+    }
+  }
+}
+
+// Workspace Data
+data "terraform_remote_state" "aws_security_group" {
+  backend = "remote"
+
+  config = {
+    organization = "rogercorp"
+    workspaces = {
+      name = "aws-security-group-prod"
+    }
+  }
+}
+
+module "aws_instance" {
+  source  = "app.terraform.io/rogercorp/aws-instance-PMR/tfe"
+  version = "1.0.3"
+
+  name   = "rchao"
+  instance_count = "3"
+  instance_type = "t2.micro"
+  owner = "rchao"
+  ttl = "3"
+
+  vpc_security_group_ids  = [data.terraform_remote_state.aws_security_group.outputs.security_group_id]
+  subnet_id               = data.terraform_remote_state.aws_vpc_prod.outputs.public_subnets[0]
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "public_ip" {
+value = module.aws_instance.public_ip
+}
+
+output "private_key_pem" {
+value = module.aws_instance.private_key_pem
+}
